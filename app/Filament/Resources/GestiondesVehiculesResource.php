ff3997ccb575072e526e03467d18085714ad6f22
Oycctem <Ousstemm@gmail.com>
2024-08-27 00:04:02 +0100

    Updated Vehicle management

--- app/Filament/Resources/GestiondesVehiculesResource.php
+++ app/Filament/Resources/GestiondesVehiculesResource.php
@@ -19,6 +19,8 @@ class GestiondesVehiculesResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
+    protected static ?string $label = 'Vehicle management';
+
     protected static ?string $navigationGroup = 'Staff';
 
     public static function form(Form $form): Form
